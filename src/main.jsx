14df13060b5f7a30b8d6a750827a4a198cab81e3
yamanpatle <yamanpatle398@gmail.com>
2025-08-16 21:21:06 +0530

    Swicted to hashrouter for gh pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
-import './styles/globals.css'
+import React from 'react';
+
+import ReactDOM from 'react-dom/client';
+
+import { HashRouter } from 'react-router-dom';
+
+import App from './App.jsx';
+
+import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
